126b8ff05f3ec6b1b1d40a4f463abd908184bcd5
hnko <henzikou@gmail.com>
2024-02-08 12:15:27 -0800

    updating styling/format

--- q2_gatk/_gatk.py
+++ q2_gatk/_gatk.py
@@ -23,8 +23,8 @@ def haplotype_caller(
     """haplotype_caller."""
     vcf = VCFIndexDirectory()
     realigned_bam = BAMIndexAlignmentDirectory()
-    for bam, bai in zip(deduplicated_bam.bam_file_paths, deduplicated_bam.bai_file_paths):
 
+    for bam, bai in zip(deduplicated_bam.bam_file_paths, deduplicated_bam.bai_file_paths):
         cmd = [
             "gatk",
             "HaplotypeCaller",
@@ -41,9 +41,12 @@ def haplotype_caller(
             "-O",
             os.path.join(str(vcf), Path(bam).stem + ".vcf"),
         ]
+
         if emit_ref_confidence:
             cmd.extend(["-ERC", str(emit_ref_confidence)])
+
         subprocess.run(cmd, check=True)
+
     return vcf, realigned_bam
 
 
@@ -54,6 +57,7 @@ def mark_duplicates(
     """mark_duplicates."""
     deduplicated_bam = BAMDirFmt()
     metrics = MetricsFile()
+
     for path, _ in sorted_bam.bams.iter_views(view_type=BAMFormat):  # type: ignore
         cmd = [
             "gatk",
@@ -65,6 +69,7 @@ def mark_duplicates(
             "-O",
             os.path.join(str(deduplicated_bam), str(path.stem) + ".bam"),
         ]
+
         try:
             subprocess.run(cmd, check=True)
         except subprocess.CalledProcessError as e:
@@ -83,6 +88,7 @@ def add_replace_read_groups(
 ) -> BAMDirFmt:
     """add_replace_read_groups."""
     sorted_bam = BAMDirFmt()
+
     for path, _ in input_bam.bams.iter_views(view_type=BAMFormat):  # type: ignore
         cmd = [
             "gatk",
@@ -102,6 +108,7 @@ def add_replace_read_groups(
             "-SM",
             str(path.stem),
         ]
+
         try:
             subprocess.run(cmd, check=True)
         except subprocess.CalledProcessError as e:
@@ -118,6 +125,7 @@ def build_bam_index(
 ) -> BAMIndexAlignmentDirectory:
     """build_bam_index."""
     bam_index = BAMIndexAlignmentDirectory()
+
     for path, _ in coordinate_sorted_bam.bams.iter_views(view_type=BAMFormat):
         bam_path = os.path.join(str(coordinate_sorted_bam), str(path))
         cmd = [
@@ -130,9 +138,11 @@ def build_bam_index(
                 str(bam_index), Path(path).stem + ".bai"
             ),  # output formatting needs to change to work with new bamindexalignmentdirectoryformat attributes
         ]
+
         try:
             subprocess.run(cmd, check=True)
             shutil.copyfile(bam_path, os.path.join(str(bam_index), os.path.basename(bam_path)))
         except subprocess.CalledProcessError as e:
             raise ValidationError("An error occurred while running GATK BuildBamIndex: %s" % str(e))
+
     return bam_index
